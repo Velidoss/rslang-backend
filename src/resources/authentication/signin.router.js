ac5727e655c9c23c989d8814a3ba9b14800409d8
Velidoss <godofthemountain09@gmail.com>
2021-04-02 20:34:12 +0300

    feat: added login through form

--- src/resources/authentication/signin.router.js
+++ src/resources/authentication/signin.router.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const { OK } = require('http-status-codes');
+const multer = require('multer');
+const path = require('path');
+const loader = multer({ dest: path.join(__dirname, 'tmp') });
 
 const userService = require('../users/user.service');
 
-router.route('/').post(async (req, res) => {
+router.post('/', loader.none(), async (req, res) => {
   const auth = await userService.authenticate(req.body);
 
   res.status(OK).json({
